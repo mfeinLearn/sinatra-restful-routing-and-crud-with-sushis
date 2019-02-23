d1350fc52ac2365079b4bb81e95977e79a28e622
github username <github email address>
2019-02-23 12:00:40 -0500

    Done.

--- app/controllers/sushis_controller.rb
+++ app/controllers/sushis_controller.rb
@@ -1,11 +1,14 @@
 class SushiController < ApplicationController
-
+  # NOTE - instance varables inside of a block is only availible inside that
+  # block and is destroyed when a redirect occur(because we are going to a different
+  # block). BUTTT an instance varable
+  # is not deleted when at that block a template is being rendered
 
   get '/' do
     erb :welcome
   end
 
-  # INDEX ROUTE - SHOW ALL THE THINGS (SUSHI)
+  # INDEX ROUTE - SHOW ALL THE THINGS (SUSHI) // read action
   # http verb get corresponds to the sinatra method get that we are invocing here
   # passing a block to indicate what we want to happen.
 
@@ -22,7 +25,7 @@ class SushiController < ApplicationController
   end
 
 
-  # NEW ROUTE - RENDER THE FORM
+  # NEW ROUTE - RENDER THE FORM // create action
   get '/sushis/new' do
     # what happens?
     # (nothing needs to happen)
@@ -31,7 +34,22 @@ class SushiController < ApplicationController
     erb :'/sushis/new'
   end
 
-  # SHOW ROUTE - SHOW ONE THING
+  #CREATE ACTION // create action - this is the action that recieves the form
+  post '/sushis' do # job of this is to take information and create a
+                    # new resource
+    # what happens?
+    # we need to use the incoming form info to
+    # create a new sushi roll!
+    @sushi = Sushi.create(params)
+    #binding.pry
+    # erb/redirect?
+    # where do we go?
+    redirect "/sushis/#{@sushi.id}"
+    # we should send a new http request and send it to the action whos job is to
+    # show this thing
+  end
+
+  # SHOW ROUTE - SHOW ONE THING // read action
   # this is a convintion for a show route
   # the id has to be something that is unique to every paticular instance of the model
   # dynamic route - a single part of multiple parts of it that is going to change
@@ -46,12 +64,38 @@ class SushiController < ApplicationController
     erb :'/sushis/show'
   end
 
-
-  #CREATE ACTION
-  post '/sushis' do
+  # what is the restful convention for building a route to render an edit form
+  # we need an http verb get which corisponds with our sinatra method (get)
+  # need to know what type of resource - /sushis/
+  # need to have a paticular resource - :id
+  # and I am editing it (the resource in question(:id))
+  get '/sushis/:id/edit' do
     # what happens?
+    @sushi = Sushi.find(params[:id])
     # erb/redirect?
     # where do we go?
+    erb :'/sushis/edit'
+  end
+
+  patch '/sushis/:id' do
+    # what happens?
+    # we need to use the incoming form info to
+    # edit an existing sushi roll!
+    @sushi = Sushi.find(params[:id])
+    @sushi.update(name: params[:name], price: params[:price], description: params[:description])
+    #binding.pry
+    # erb/redirect?
+    # where do we go?
+    redirect "/sushis/#{@sushi.id}"
+    # we should send a new http request and send it to the action whos job is to
+    # show this thing
+  end
+
+  delete '/sushis/:id' do
+    @sushi = Sushi.find(params[:id])
+    @sushi.destroy
+
+    redirect '/sushis'
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190222080057) do
+ActiveRecord::Schema.define(version: 20190222195231) do
 
   create_table "sushis", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "price"
+    t.string   "description"
   end
 
 end
